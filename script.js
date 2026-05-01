19e909ac9d8e289dae6ef15453d0af6a274235a8
geizzi <geizzicavalcante@gmail.com>
2026-05-01 16:07:53 -0300

    Ajustes no site da confeitaria: avaliações e melhorias visuais

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+    // TROCA DE IMAGENS (produtos)
     const imagens = document.querySelectorAll(".img-troca");
 
     imagens.forEach(function (img) {
@@ -17,4 +18,24 @@ document.addEventListener("DOMContentLoaded", function () {
 
     });
 
+    // CARROSSEL DO TOPO
+    let index = 0;
+    const slides = document.querySelectorAll(".slide");
+
+    function mostrarSlide(i) {
+        slides.forEach(slide => slide.classList.remove("ativo"));
+        slides[i].classList.add("ativo");
+    }
+
+    document.querySelector(".next").addEventListener("click", () => {
+        index = (index + 1) % slides.length;
+        mostrarSlide(index);
+    });
+
+    document.querySelector(".prev").addEventListener("click", () => {
+        index = (index - 1 + slides.length) % slides.length;
+        mostrarSlide(index);
+    });
+
 });
+console.log("Slides encontrados:", document.querySelectorAll(".slide").length);
